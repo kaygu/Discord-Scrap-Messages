70162fd52432922e02126f59d772c02216c852e3
kaygu <camille.deneef@gmail.com>
2023-05-18 02:29:37 +0200

    add more options to config file, add on_message event

--- scrap_history.py
+++ scrap_history.py
@@ -4,44 +4,51 @@ import os
 
 import discord
 
-DEFAULT_FOLDER_NAME = "logs"
-CSV_SEP = "\t"
-
 client = discord.Client()
 
 with open('config.json') as f:
     config = json.load(f)
 
-def save_line(out, message):
-    #TODO: Escape special characters
-    #TODO: Change embeds & attachements to lists
-    lines = []
-    lines.append("{0}".format(message.author.name))
-    lines.append("{0}".format(message.author.id))
-    lines.append("{0}".format(message.created_at))
-    lines.append("{0}".format(message.id))
-    lines.append("{0}".format(message.content))
-    atc_line = []
+LOG_FOLDER = config.get("log_folder", "logs")
+CSV_SEP = config.get("csv_separator", "\t")
+
+def extract_message(message):
+    # TODO: Escape special characters
+    # TODO: Other options than CSV. json should be a feature next
+    line = []
+    values = []
+    values.append("{0}".format(message.author.name))
+    values.append("{0}".format(message.author.id))
+    values.append("{0}".format(message.created_at))
+    values.append("{0}".format(message.id))
+    values.append("{0}".format(message.content))
+    atc_val = []
     if message.attachments:
         for atch in message.attachments:
-            atc_line.append(atch.url)
-    lines.append(json.dumps(atc_line))
-    embed_line = []
+            atc_val.append(atch.url)
+    values.append(json.dumps(atc_val))
+    embed_val = []
     if message.embeds:
         for embed in message.embeds:
-            embed_line.append(embed.to_dict())
-    lines.append(json.dumps(embed_line))
+            embed_val.append(embed.to_dict())
+    values.append(json.dumps(embed_val))
      
 
-    for line in lines:
-        out.write(line + CSV_SEP)
-    out.write('\n')
+    for val in values:
+        line.append(val + CSV_SEP)
+    line.append('\n')
+    return "".join(line)
+
+def save_line(out, message):
+    # TODO: Batch file writing
+    # TODO: Change file every x messages to avoid files with gigabytes of data
+    out.write(extract_message(message))
     
 
 async def get_logs(channel: discord.TextChannel):
     try:
         print(f"\n{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Gettings message history for {Fore.WHITE}{channel.name}")
-        with open("{0}/{1}_{2}_{3}.csv".format(config.get("log_folder", DEFAULT_FOLDER_NAME), channel.guild.name, channel.name, channel.id), 'w', encoding="utf-8") as f:
+        with open("{0}/{1}_{2}_{3}.csv".format(LOG_FOLDER, channel.guild.name, channel.name, channel.id), 'w', encoding="utf-8") as f:
             async for line in channel.history(limit=config.get("amount"), oldest_first=True):
                 save_line(f, line)
 
@@ -51,7 +58,7 @@ async def get_logs(channel: discord.TextChannel):
 
 def Init():
     try:
-        os.mkdir(config.get("log_folder", DEFAULT_FOLDER_NAME))
+        os.mkdir(LOG_FOLDER)
     except FileExistsError:
         pass
     if not config.get('token'):
@@ -75,6 +82,7 @@ async def on_ready():
     print(f'{Fore.LIGHTBLACK_EX}We have logged in as {client.user}')
     guild_id = config.get("guild")
     channel_ids = config.get("channels")
+    global channels # Shame on me, but this is the only way I found until I rewrite my script 
     channels = []
     try:
         if guild_id:
@@ -89,11 +97,20 @@ async def on_ready():
                     raise Exception("Channel ID is invalid")
                 if channel not in channels:
                     channels.append(channel)
+                else:
+                    print(f"\n{Fore.WHITE}[ {Fore.YELLOW}W {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Channel ID {Fore.WHITE}{channel} {Fore.LIGHTBLACK_EX}is already in the channel list\n")
         for channel in channels:
             await get_logs(channel)
     except Exception as e:
         print(f"{Fore.WHITE}[ {Fore.RED}E {Fore.WHITE}] {Fore.WHITE}{e}")
-    await client.close()
+
+@client.event
+async def on_message(message):
+    # Live Listener
+    # TODO: Save messages here, but only once logs history is gathered (so no duplicates)
+    if message.channel in channels:
+        # Log message here
+        print(f"{Fore.WHITE}[ {Fore.GREEN}@ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Messages in channel {Fore.WHITE}{message.channel.name} {Fore.LIGHTBLACK_EX}recieved")
     
     
 
